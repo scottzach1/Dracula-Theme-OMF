a1b0a61abe7d51ca1d50c9ef6d8b0e0d3d2f5130
scottzach1 <scottzach1@myvuw.ac.nz>
2020-04-03 00:47:44 +1300

    Minor tweaks

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -12,15 +12,16 @@ function fish_prompt
   set -l cyan (set_color -o cyan)
   set -l yellow (set_color -o yellow)
   set -l red (set_color -o red)
-  set -l blue (set_color -o blue)
+  set -l purple (set_color -o blue)
   set -l green (set_color -o green)
   set -l normal (set_color normal)
 
   if test $last_status = 0
-      set status_indicator "$green✔︎ "
+    set arrow "$green➜ "
   else
-      set status_indicator "$red✗ "
+    set arrow "$red➜ "
   end
+  
   set -l cwd $cyan(basename (prompt_pwd))
 
   if [ (_git_branch_name) ]
@@ -30,7 +31,7 @@ function fish_prompt
       set git_info "$normal ($red$git_branch$normal)"
     else
       set -l git_branch (_git_branch_name)
-      set git_info "$normal ($blue$git_branch$normal)"
+      set git_info "$normal ($purple$git_branch$normal)"
     end
 
     if [ (_is_git_dirty) ]
