840ac29e1d8cee2eccb1c60120ca3320c448ff4f
scottzach1 <scottzach1@myvuw.ac.nz>
2020-04-02 18:55:35 +1300

    Minor tweaks

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,5 +1,5 @@
-# name: scottzach1
-#
+
+# name: eclm
 function _git_branch_name
   echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
 end
@@ -9,34 +9,34 @@ function _is_git_dirty
 end
 
 function fish_prompt
-  # Set variables
   set -l last_status $status
   set -l cyan (set_color -o cyan)
   set -l yellow (set_color -o yellow)
   set -l red (set_color -o red)
-  set -l purple (set_color -o purple)
+  set -l blue (set_color -o blue)
   set -l green (set_color -o green)
   set -l normal (set_color normal)
 
-  # Set status arrow
   if test $last_status = 0
-    set arrow "$green➜ "
+      set status_indicator "$green✔︎ "
   else
-    set arrow "$red➜ "
+      set status_indicator "$red✗ "
   end
+  set -l cwd $cyan(basename (prompt_pwd))
 
-  # Set current working directory
-  set -l cwd $purple(basename (prompt_pwd))
-
-  # Set git info
   if [ (_git_branch_name) ]
-    set -l git_branch $cyan(_git_branch_name)
-    set git_info "$cyan ($_git_branch)"
+
+    if test (_git_branch_name) = 'master'
+      set -l git_branch (_git_branch_name)
+      set git_info "$normal ($red$git_branch$normal)"
+    else
+      set -l git_branch (_git_branch_name)
+      set git_info "$normal ($blue$git_branch$normal)"
+    end
 
     if [ (_is_git_dirty) ]
-      set git_info "$git_info $yellow✗"
-    else
-      set git_info "$git_info $green✔︎"
+      set -l dirty "$yellow ✗"
+      set git_info "$git_info$dirty"
     end
   end
 
@@ -45,6 +45,5 @@ function fish_prompt
     echo The last command took (math "$CMD_DURATION/1000") seconds.
   end
 
-  # Output to console
-  echo -n -s $arrow ' ' $cwd $git_info $normal ' '
+  echo -n -s $status_indicator $cwd $git_info $normal ' '
 end
